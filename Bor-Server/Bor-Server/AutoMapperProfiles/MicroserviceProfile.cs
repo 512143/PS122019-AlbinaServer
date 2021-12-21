02c15ad59930f253bf81bc1c9614e97ab35c8f3b
TitikUniverse <semyon.titov.203@mail.ru>
2021-12-21 18:30:50 +0300

    Начал APi юзера

---
+++ Bor-Server/Bor-Server/AutoMapperProfiles/MicroserviceProfile.cs
@@ -0,0 +1,15 @@
+﻿using Albina.BusinesLogic.Core.Models;
+using AutoMapper;
+using Bor.Core.Models;
+
+namespace Bor_Server.AutoMapperProfiles
+{
+    public class MicroserviceProfile : Profile
+    {
+        public MicroserviceProfile()
+        {
+            CreateMap<UserInformationBlo, UserInformationDto>();
+            CreateMap<UserIdentityBlo, UserIdentityDto>();
+        }
+    }
+}
---
+++ Bor-Server/Bor-Server/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bor.DataAccess.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Bor.Core.Models;
+using Albina.BusinesLogic.Core.Interfaces;
+using AutoMapper;
+
+namespace Bor_Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+
+        public UserController(IUserService userService, IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [HttpPost("/auth")]
+        public async Task<ActionResult<UserInformationDto>> Auth([FromBody] UserIdentityDto userIdentityDto)
+        {
+            _userService.Auth();
+        }
+    }
+}
